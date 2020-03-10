377d0dcc318a8211f85160ffe0044076023f8da9
Wyatt Howe <wyatt@wyatthowe.net>
2020-03-10 01:07:50 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pbnj - Parse by name javascript
+Custom math.js parsing to support evaluating recursive equations safely and efficiently.
---
+++ index.js
@@ -0,0 +1,31 @@
+const mathjs = require('mathjs');
+
+function parse(definitions, context) {
+  if (context == null) {
+    context = {};
+  }
+
+  /*
+  Expand context
+  */
+
+  return context;
+}
+
+function evaluate(expression, context) {
+  if (context == null) {
+    context = {};
+  }
+
+  let value;
+  /*
+  Evaluate expression in context
+  */
+
+  return value;
+}
+
+module.exports = {
+  parse: parse,
+  evaluate: evaluate
+}
