dcc070d1fb5645760ec2a4aafc0bd0fe30dd38a8
Wyatt Howe <wyatt@wyatthowe.net>
2020-03-13 02:52:22 -0400

    add more comments

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
 const mlib = require('mathjs');
-const mathjs = mlib.create(mlib.all)
+const mathjs = mlib.create(mlib.all);
 
+/* Rewrite a recurance's initial conditions in iff form  */
+const get_rec_def = function (recurance, conditions) {
+  const fn = mathjs.parse(recurance);
+  const name = fn.name;
+  const param = fn.params[0];  // TODO: support mulitvariable recurances
+  const expr = fn.expr;
+
+  const [fc, f_c] = mathjs.parse(conditions.split('='));  // f(c)
+  const c = fc.args[0];
+
+  // Eg. f     (    n    ) = iff(    n    ,     n    >=  1  , "f(n/2)+1",    0   )
+  return name+'('+param+') = iff('+param+', '+param+'>='+c+', "'+expr+'", '+f_c+')';
+};
+
+/* Pre-parse an expression string */
 const preparse = function (expression) {
-  // console.log('preparse', expression);
-  // const fn = mathjs.parse(recurance);
-  // const name = fn.name;
-  // const params = fn.params;
-  // const expr = fn.expr;
-  //
-  // let [fc, f_c] = mathjs.parse(conditions.split('='));  // f(c)
-  // let c = fc.args[0];
+  const rec_def = /* to do */ false;
+  if (rec_def) {
+    expression = get_rec_def(expression);
+  }
 
   return expression;
 };
 
-var mem = [];
+/* Memoized ternary if else */
+var mem = {};
 const iff = function (args, _, scope) {
   const memotag = args.map(String).join('')+JSON.stringify(scope);
   let memo = mem[memotag];
   if (memo === undefined) {
     const [above_base_case, f, base_val] = args;
+
+    // iff base condition not met, recurse, else, stop
     memo = above_base_case.evaluate(scope) ? f.evaluate(scope) : base_val.evaluate(scope);
+
     mem[memotag] = memo;
   }
   return memo;
 }; iff.rawArgs = true;
 
-mathjs.import({'iff': iff})
+mathjs.import({'iff': iff});  // Load the lazily evaluated definition
 
+/* Create custom math.js parser */
 const Parser = function (context) {
   const parser = mathjs.parser();
 
